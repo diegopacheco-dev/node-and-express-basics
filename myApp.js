151db8e422be439ecda2fcf39032f165335f21d8
Diego <ericksonder017@gmail.com>
2021-12-10 10:04:50 -0500

    Agregando variables de entorno con dotenv

--- myApp.js
+++ myApp.js
@@ -1,39 +1,15 @@
-var express = require('express');
+require('dotenv').config()
+
+var express = require("express");
 var app = express();
 
+app.get("/json", (req, res) => {
+  console.log(process.env.MESSAGE_STYLE);
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
