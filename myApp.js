aad2ecd5b674eb23d2fcba37cf1b0fc8c7ce5ee5
Diego <ericksonder017@gmail.com>
2021-12-10 10:56:52 -0500

    Refactorizando logica del json response

--- myApp.js
+++ myApp.js
@@ -1,15 +1,15 @@
-require('dotenv').config()
+require("dotenv").config();
 
 var express = require("express");
 var app = express();
 
 app.get("/json", (req, res) => {
   console.log(process.env.MESSAGE_STYLE);
+  const jsonResponse = { message: "Hello json" };
   if (process.env.MESSAGE_STYLE === "uppercase") {
-    res.json({ message: "HELLO JSON" });
-  } else {
-    res.json({ message: "Hello json" });
+      jsonResponse.message = jsonResponse.message.toUpperCase()
   }
+  res.json(jsonResponse)
 });
 
 module.exports = app;
