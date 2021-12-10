210d9c8ec466dddf08f31dc472170a8e8ba03172
Diego <ericksonder017@gmail.com>
2021-12-10 11:32:29 -0500

    Agregando middleware

--- myApp.js
+++ myApp.js
@@ -4,12 +4,20 @@ var express = require("express");
 var app = express();
 
 app.get("/json", (req, res) => {
-  console.log(process.env.MESSAGE_STYLE);
-  const jsonResponse = { message: "Hello json" };
+  let jsonResponse = { "message": "Hello json" };
   if (process.env.MESSAGE_STYLE === "uppercase") {
-      jsonResponse.message = jsonResponse.message.toUpperCase()
+      jsonResponse.message = jsonResponse.message.toUpperCase();
   }
-  res.json(jsonResponse)
+  res.json(jsonResponse);
 });
+
+app.use('/', (req, res, next) => {
+    const method = req.method;
+    const path = req.path;
+    const ip = req.ip;
+
+    console.log(`${method} ${path} - ${ip}`)
+    next()
+})
 
 module.exports = app;
